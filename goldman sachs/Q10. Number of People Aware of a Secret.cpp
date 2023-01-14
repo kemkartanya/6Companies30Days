cd440ecac19189c7cf93f7ad1b279caaea8f1097
Tanya Kemkar <tanyakemkar@gmail.com>
2023-01-14 12:07:24 +0530

    Create Q10. Number of People Aware of a Secret.cpp

---
+++ goldman sachs/Q10. Number of People Aware of a Secret.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int peopleAwareOfSecret(int n, int delay, int forget) {
+        vector<long> dp(n + 1); // dp[i] denotes no. of new people who discovered secret on day i
+        dp[1] = 1;
+        int share = 0, mod = 1e9 + 7;
+        for(int i=2; i<=n; i++) {
+            dp[i] = share = (share + dp[max(i-delay,0)] - dp[max(i-forget,0)] + mod) % mod;
+        }
+
+        int res = 0;
+        for(int i=n-forget+1; i<=n; i++) {
+            res = (res + dp[i]) % mod;
+        }
+        return res;
+    }
+};
